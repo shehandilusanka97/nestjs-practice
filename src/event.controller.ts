5b683b43807f251a7b624c238d4c16d71020ffcf
shehandilusanka97 <kshehandilusanka97@gmail.com>
2024-06-30 13:42:47 +0530

    event-controller updated

--- src/event.controller.ts
+++ src/event.controller.ts
@@ -24,19 +24,34 @@ export class EventController {
   @Get(':id')
   findOne(@Param('id') id) {
     const event = this.events.find((event) => event.id === parseInt(id));
+    return event;
   }
 
   @Post()
   create(@Body() input: CreateEventDto) {
-    return input;
+    const event = {
+      ...input,
+      when: new Date(input.when),
+      id: this.events.length + 1,
+    };
+    this.events.push(event);
+    return event;
   }
 
   @Patch(':id')
   update(@Param('id') id, @Body() input: UpdateEventDto) {
-    return input;
+    const index = this.events.findIndex((event) => event.id === parseInt(id));
+    this.events[index] = {
+      ...this.events[index],
+      ...input,
+      when: input.when ? new Date(input.when) : this.events[index].when,
+    };
+    return this.events[index];
   }
 
   @Delete(':id')
   @HttpCode(204)
-  remove(@Param('id') id) {}
+  remove(@Param('id') id) {
+    this.events = this.events.filter(event => event.id !== parseInt(id));
+  }
 }
